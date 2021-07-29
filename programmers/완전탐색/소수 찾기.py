9d2317d15bce97b15844ed8597e8247793cf218f
thdals83 <thdals83@naver.com>
2021-07-29 16:21:35 +0900

    프로그래머스 코테연습

--- programmers/완전탐색/소수 찾기.py
+++ programmers/완전탐색/소수 찾기.py
@@ -30,15 +30,3 @@ if __name__ == "__main__":
     n = "17"
     n1 = "011"
     print(solution(n1))
-
-'''
-- 순열을 구하는 함수 permutatinos 을 쓸 경우 모든 경우의 수를 다 
-생각하기 때문에 결국 시간초과가 뜨고 만다.
-from itertools import permutations
-def solution(num):
-    res = []
-    num1=list(map(str,num))
-    for i in permutations(num1,len(num)):
-        res.append(''.join(i))
-    return str(max(res))
-'''
