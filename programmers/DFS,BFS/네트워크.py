b702db3f35229f075406334e8c21de39d8a1edbe
thdals83 <thdals83@naver.com>
2021-08-13 15:02:04 +0900

    프로그래머스 코테연습

---
+++ programmers/DFS,BFS/네트워크.py
@@ -0,0 +1,26 @@
+def solution(n, com):
+    answer = 0
+    bfs = []
+    visited = [0] * n
+
+    while 0 in visited:
+        x = visited.index(0)
+        bfs.append(x)
+        visited[x] = 1
+
+        while bfs:
+            node = bfs.pop(0)
+            visited[node] = 1
+            for i in range(n):
+                if visited[i] == 0 and com[node][i] == 1:
+                    bfs.append(i)
+                    visited[i] = 1
+        answer += 1
+
+    return answer
+
+
+if __name__ == "__main__":
+    n = 3
+    computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+    print(solution(n, computers))
