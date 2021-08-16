894eb970f839c96b2b030ab5df0aac7e469436b3
thdals83 <thdals83@naver.com>
2021-08-16 15:07:18 +0900

    프로그래머스 코테연습

---
+++ programmers/BinarySearch/입국심사.py
@@ -0,0 +1,33 @@
+def solution(n, times):
+    # binary search를 위한 sorting
+    times.sort()
+    # 기다리는 사람 1명, 심사관 1분일 때 최소시간
+    min_time = 1
+    # 기다리는 사람 모두 심사시간이 가장 긴 심사관에게 갈 때 최대시간
+    max_time = times[len(times) - 1] * n
+
+    answer = max_time
+
+    while min_time <= max_time:
+        mid_time = int((min_time + max_time) / 2)
+
+        sum = 0
+        # mid time일 때, 몇 명 검사할 수 있을지 체크
+        for i in range(len(times)):
+            sum += int(mid_time / times[i])
+
+        # n명보다 더 많은 사람을 검사할 수 있을 때 최대시간 감소
+        if sum >= n:
+            if answer > mid_time:
+                answer = mid_time;
+            max_time = mid_time - 1
+        # n명보다 더 적은 사람을 검사할 수 있을 때 최소시간 증가
+        else:
+            min_time = mid_time + 1
+    return answer
+
+
+if __name__ == "__main__":
+    n = 6
+    t = [7, 10]
+    print(solution(n,t))
--- programmers/DFS,BFS/단어 변환.py
+++ programmers/DFS,BFS/단어 변환.py
@@ -1,3 +1,12 @@
+'''
+1. 찾는 단어가 없는 경우 예외처리
+
+현재 단어 리스트: word_list
+word_list에 단어를 담고 해당 단어에서
+변환이 될 수 있는 단어들을 diff_list에 담는다. (words 리스트에선 삭제)
+diff_list랑 비교해서 target나오면 끝
+안나오면 diff_list 값을 word_list로 담아준다
+'''
 def solution(begin, target, words):
     if target not in words: return 0
 
