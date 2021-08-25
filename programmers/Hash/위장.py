2d3b0772823f677377a98ec289ab2c114fd5fc12
thdals83 <thdals83@naver.com>
2021-08-25 15:12:42 +0900

    프로그래머스 알고리즘

--- programmers/Hash/위장.py
+++ programmers/Hash/위장.py
@@ -1,25 +1,18 @@
-'''
-def solution(cl):
-    arr={}
-    for i in cl:
+
+
+def solution(clothes):
+    arr ={}
+    for i in clothes:
         if i[-1] in arr:
-            arr[i[-1]] = arr[i[-1]] + 1
+            arr[i[-1]] += 1
         else:
             arr[i[-1]] = 1
 
     sum = 1
-    for i  in arr.values():
-        sum = sum * (i+1)
+    for i in arr.values():
+        sum *= (i+1)
+    return sum -1
 
-    return sum-1
-'''
-
-def solution(clothes):
-    from collections import Counter
-    from functools import reduce
-    cnt = Counter([kind for name, kind in clothes])
-    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
-    return answer
 
 
 if __name__ =="__main__":
