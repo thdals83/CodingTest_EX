fecb63bd5f8af3a770d8b80196895b34b4da7efb
thdals83 <thdals83@naver.com>
2021-08-25 15:44:22 +0900

    프로그래머스 알고리즘

--- programmers/Hash/베스트앨범.py
+++ programmers/Hash/베스트앨범.py
@@ -1,22 +1,25 @@
-def solution(genres,plays):
-    d = {}
-    playsDict = {}
 
-    x = 0
-    for i, j in zip(genres, plays):
-        playsDict[i] = playsDict.get(i, 0) + j
-        d[i] = d.get(i, []) + [(j, x)]
-        x = x + 1
-    arr = sorted(playsDict.items(), key=lambda x: x[1], reverse=True)
-    res = []
-    for i in range(len(arr)):
-        arr2 = sorted(d[arr[i][0]], key=lambda x: x[0], reverse=True)
-        if(len(arr2)==1):res.append(arr2[0][1])
+def solution(genres,plays):
+    arr = {}
+    sum = {}
+    i = 0
+    for x,y in zip(genres,plays):
+        arr[x] = arr.get(x,[]) + [(y,i)]
+        sum[x] = sum.get(x,0) + y
+        i += 1
+    max = sorted(sum.items(), key=lambda x:x[1],reverse=True)
+    res =[]
+    for i in range(len(max)):
+        max2 = sorted(arr[max[i][0]], key=lambda x:x[0],reverse=True)
+        if len(max2) < 2:
+            res.append(max2[0][1])
         else:
-            res.append(arr2[0][1])
-            res.append(arr2[1][1])
+            res.append(max2[0][1])
+            res.append(max2[1][1])
 
     return res
+
+
 
 if __name__ =="__main__":
     g = ["classic", "pop", "classic", "classic", "pop"]
