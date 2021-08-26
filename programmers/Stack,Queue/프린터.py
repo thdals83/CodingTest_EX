e5dcb4ad0695b9a765cbe5c215a939f0da37535a
thdals83 <thdals83@naver.com>
2021-08-26 14:16:00 +0900

    프로그래머스 알고리즘

--- programmers/Stack,Queue/프린터.py
+++ programmers/Stack,Queue/프린터.py
@@ -1,24 +1,28 @@
 def solution(p,l):
-    res = 0
-    im = max(p)
-    while True:
-        x = p.pop(0)
-        if im == x:
-            res = res + 1
-            if l == 0: break
+    arr = []
+    for i in range(len(p)):
+        arr.append([p[i],i])
+
+    i = 0
+    res = 1
+    while len(arr) != 0:
+        if i == len(arr): i = 0
+
+        if arr[0][0] == max(t[0] for t in arr):
+            if arr[0][1] == l: return res
             else:
-                l = l - 1
-                im = max(p)
+                arr.pop(0)
+                res += 1
         else:
-            p.append(x)
-            if l == 0: l = len(p) -1
-            else: l = l -1
+            tmp = arr.pop(0)
+            arr.append(tmp)
 
     return res
 
 
 
 if __name__ =="__main__":
-    p = [1, 1, 9, 1, 1, 1]
+    p = [2, 1, 3, 2]
+    p1 = [1, 1, 9, 1, 1, 1]
     l = 0
-    print(solution(p,l))
+    print(solution(p1,l))
