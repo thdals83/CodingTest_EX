00165e61a7ff502b60289b7256b7d72ebe0f0f69
thdals83 <thdals83@naver.com>
2021-08-26 18:17:22 +0900

    프로그래머스 알고리즘

--- programmers/Stack,Queue/다리를 지나는 트럭.py
+++ programmers/Stack,Queue/다리를 지나는 트럭.py
@@ -1,29 +1,31 @@
+# 경과시간 1씩 추가
+# 건너고 있는 트럭 (ing) 1씩 증가
+# 대기 트럭에 트럭이 있는 경우
+# => 현재 ing 와 대기트럭을 더했을 때 weight보다 작은 경우
+#  => ing.apppend 트럭(0)
+# 대기 트럭에 트럭이 없는 경우
+#
 def solution(length,weight,truck):
     answer = 1
     ing = []
-    timeset = []
-    ing.append(truck.pop(0))
-    timeset.append(0)
+    ing.append([truck.pop(0),0])
 
-    while True:
-        if len(ing) == 0: break
+    while len(ing) != 0:
         answer += 1
 
-        for i in range(len(timeset)):
-            timeset[i] += 1
+        for i in range(len(ing)):
+            ing[i][1] += 1
 
-        if timeset[0] == length:
-            ing.pop(0), timeset.pop(0)
+        if ing[0][1] == length: ing.pop(0)
 
         if len(truck) != 0:
-            if sum(ing) + truck[0] <= weight:
-                ing.append(truck.pop(0))
-                timeset.append(0)
+            if sum(x[0] for x in ing)+ truck[0] <= weight:
+                ing.append([truck.pop(0),0])
 
     return answer
 
 if __name__ =="__main__":
-    l = 100
-    w = 100
-    t = [10,10,10,10,10,10,10,10,10,10]
+    l = 2
+    w = 10
+    t = [7,4,5,6]
     print(solution(l,w,t))
