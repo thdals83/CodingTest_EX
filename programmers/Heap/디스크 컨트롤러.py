5eb499e21306de96d8c142eecaf2bd9ff5a450ce
thdals83 <thdals83@naver.com>
2021-08-27 14:27:34 +0900

    프로그래머스 알고리즘

--- programmers/Heap/디스크 컨트롤러.py
+++ programmers/Heap/디스크 컨트롤러.py
@@ -1,24 +1,24 @@
 import heapq
 
 def solution(jobs):
-    answer = 0
-    start = 0  # 현재까지 진행된 작업 시간
-    length = len(jobs)
+    answer = len(jobs)
+    res = 0
+    time = 0
 
-    jobs = sorted(jobs, key=lambda x: x[1])  # 소요시간 우선 정렬
-
+    jobs = sorted(jobs, key = lambda x:x[1])
     while len(jobs) != 0:
+
         for i in range(len(jobs)):
-            if jobs[i][0] <= start:
-                start += jobs[i][1]
-                answer += start - jobs[i][0]
+            if jobs[i][0] <= time:
+                time += jobs[i][1]
+                res += time - jobs[i][0]
                 jobs.pop(i)
                 break
-            # 해당시점에 아직 작업이 들어오지 않았으면 시간 ++
-            if i == len(jobs) - 1:
-                start += 1
 
-    return answer // length
+            if i == len(jobs) - 1:
+                time += 1
+
+    return res//answer
 
 if __name__ =="__main__":
     j = [[0, 3], [1, 9], [2, 6]]
