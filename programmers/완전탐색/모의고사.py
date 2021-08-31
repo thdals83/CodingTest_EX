0d2d3119c3eccf062d287fecfe195585f1d58bdb
thdals83 <thdals83@naver.com>
2021-08-31 14:24:05 +0900

    프로그래머스 알고리즘

--- programmers/완전탐색/모의고사.py
+++ programmers/완전탐색/모의고사.py
@@ -1,23 +1,26 @@
 def solution(arr):
+    x = [1, 2, 3, 4, 5]
+    y = [2, 1, 2, 3, 2, 4, 2, 5]
+    z = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     res = [0, 0, 0]
+
+    n = 0
+    for i in arr:
+        if i == x[n % len(x)]:
+            res[0] += 1
+        if i == y[n % len(y)]:
+            res[1] += 1
+        if i == z[n % len(z)]:
+            res[2] += 1
+        n +=1
+
     answer = []
-    check =[0,0,0]
-    A = [1, 2, 3, 4, 5]
-    B = [2, 1, 2, 3, 2, 4, 2, 5]
-    C = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-
-    for i in range(len(arr)):
-        if A[check[0]] == arr[i]: res[0] = res[0] + 1
-        if B[check[1]] == arr[i]: res[1] = res[1] + 1
-        if C[check[2]] == arr[i]: res[2] = res[2] + 1
-
-        for i in range(3):
-            check[i] = check[i] + 1
-            if check[0] == 5: check[0] = 0
-            if check[1] == 8 : check[1] = 0
-            if check[2] == 10 : check[2] = 0
-
     for i in range(len(res)):
-        if res[i] == max(res): answer.append(i+1)
+        if res[i] == max(res):
+            answer.append(i+1)
 
     return answer
+
+if __name__ == "__main__":
+    n1 = [1,3,2,4,2]
+    print(solution(n1))
