f3164c20dca4c34184b8bc2c0a8cc0ce82804b82
thdals83 <thdals83@naver.com>
2021-09-06 11:00:00 +0900

    프로그래머스 알고리즘

--- programmers/Greedy/섬 연결하기.py
+++ programmers/Greedy/섬 연결하기.py
@@ -1,45 +1,3 @@
-'''
-크루스칼 알고리즘 정석
-'''
-def solution(n, costs):
-    answer = 0
-
-    # 특정 원소가 속한 집합을 찾기
-    def find_parent(parent, x):
-        # 루트가 아니라면 찾을 때까지 재귀 호출
-        if parent[x] != x:
-            parent[x] = find_parent(parent, parent[x])
-        return parent[x]
-
-    # 두 원소가 속한 집합을 찾기
-    def union_parent(parent, a, b):
-        a = find_parent(parent, a)
-        b = find_parent(parent, b)
-        if a < b:
-            parent[b] = a
-        else:
-            parent[a] = b
-
-    # 부모 테이블
-    parent = [0] * n
-    # 부모를 자기 자신으로 초기화
-    for i in range(n):
-        parent[i] = i
-
-    # 크루스칼 알고리즘을 위해 비용순으로 정렬
-    costs = sorted(costs, key=lambda x: x[2])
-
-    # 크루스칼 알고리즘 수행
-    # 간선을 하나씩 확인하며
-    for edge in costs:
-        a, b, cost = edge
-        # 사이클이 발생하지 않는 경우에만 집합에 포함
-        if find_parent(parent, a) != find_parent(parent, b):
-            union_parent(parent, a, b)
-            answer += cost
-
-    return answer
-
 
 '''
 Kruskal 알고리즘 사용 하지만 정석대로 사용하진 않은 방법
@@ -62,9 +20,54 @@ def solution(n, costs):
                 break
     return answer
 '''
+'''
+크루스칼 알고리즘 정석
+1. 간선의 길이가 짧은 순으로 정렬
+2. 간선을 선택 하며 => 사이클이 발생하지 않는지 확인 (union find 기법)
+3. union find 기법 => 추가하려는 간선의 양 끝점이 같은 집합에 있지 않아야 함
+'''
+
+def solution(n, costs):
+    answer = 0
+
+    #특정 원소가 속한 집합을 찾기기
+    def find_parent(x):
+        # 루트가 아니라면 찾을 때 까지 재귀 호출
+        if parent[x] != x:
+            parent[x] = find_parent(parent[x])
+        return parent[x]
+
+    def union_parent(a,b):
+        a = find_parent(a)
+        b = find_parent(b)
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+
+    # 부모 테이블
+    parent = [0] * n
+    # 부모 테이블 자기 자신으로 초기화
+    for i in range(n): parent[i] = i
+
+    #스루스칼 알고리즘 비용순 정렬
+    costs.sort(key = lambda x:x[2])
+    print(costs)
+    #크루스칼 알고리즘 수행
+    #간선을 하나씩 확인
+    for edge in costs:
+        a, b, cost = edge
+        if find_parent(a) !=find_parent(b):
+            union_parent(a,b)
+            answer += cost
+        # 사이클이 발생하지 않는 경우에만 집합에 포함한다.
+    return answer
+
+
+
 
 
 if __name__ == "__main__":
-    n = 7
-    costs = [[0,5,10],[3,4,22],[1,6,15],[1,2,16],[2,3,12],[3,6,18],[4,5,27],[4,6,25],[0,1,29]]
+    n = 4
+    costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
     print(solution(n,costs))
