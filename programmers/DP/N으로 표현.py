bac8462b63f1cd22eeca9fd1c56a8e7d8140c7d0
thdals83 <thdals83@naver.com>
2021-09-06 14:52:55 +0900

    프로그래머스 알고리즘

--- programmers/DP/N으로 표현.py
+++ programmers/DP/N으로 표현.py
@@ -1,23 +1,3 @@
-#
-def solution(n, number):
-
-    arr = [set() for i in range(8)]
-    for i in range(0,8):
-        if int(str(n)*(i+1)) == number: return i+1
-        arr[i].add(int(str(n)*(i+1)))
-
-    for i in range(1,8):
-        for j in range(i):
-            for op1 in arr[j]:
-                for op2 in arr[i-j-1]:
-                    arr[i].add(op1 + op2)
-                    arr[i].add(op1 - op2)
-                    arr[i].add(op1 * op2)
-                    if op2 != 0: arr[i].add(op1 // op2)
-        print(arr[j])
-
-        if number in arr[i]: return i+1
-    return -1
 '''
 def solution(n,number):
     arr = [set() for i in range(8)]
@@ -40,8 +20,23 @@ def solution(n,number):
     return -1
 '''
 
+def solution(n, number):
+    arr = [set() for i in range(8)]
+    for i in range(8):
+        if int(str(n)*(i+1)) == number: return i+1
+        arr[i].add(int(str(n)*(i+1)))
 
+    for i in range(1,8):
+        for j in range(i):
+            for op1 in arr[j]:
+                for op2 in arr[i-j-1]:
+                    arr[i].add(op1 + op2)
+                    arr[i].add(op1 - op2)
+                    arr[i].add(op1 * op2)
+                    if op2 != 0: arr[i].add(op1 // op2)
 
+        if number in arr[i]: return i+1
+    return -1
 
 
 if __name__ == "__main__":
