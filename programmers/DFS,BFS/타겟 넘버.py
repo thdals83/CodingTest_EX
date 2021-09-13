270de2f2015c30f415d902e39883c4611f9b93fe
thdals83 <thdals83@naver.com>
2021-09-13 15:09:36 +0900

    프로그래머스 알고리즘

--- programmers/DFS,BFS/타겟 넘버.py
+++ programmers/DFS,BFS/타겟 넘버.py
@@ -1,4 +1,4 @@
-
+'''
 def solution(n,target):
     arr = [0]
     for i in n:
@@ -9,6 +9,32 @@ def solution(n,target):
         arr = tmp
 
     return arr.count(target)
+'''
+from collections import deque
+
+def solution(n,target):
+    answer = 0
+    queue = deque()
+    num = len(n)
+    queue.append([n[0],0])
+    queue.append([-1*n[0],0])
+
+
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < num:
+            queue.append([temp + n[idx], idx])
+            queue.append([temp - n[idx], idx])
+        else:
+            if temp == target:
+                answer +=1
+
+
+
+    return answer
+
+
 
 
 if __name__ == "__main__":
