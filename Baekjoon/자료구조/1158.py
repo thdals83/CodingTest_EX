9116ea41b59bfbb86e46f5288820b6109d1e3de0
thdals83 <thdals83@naver.com>
2021-09-28 15:31:24 +0900

    백준 알고리즘

---
+++ Baekjoon/자료구조/1158.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    x,y = map(int,input().split())
+    arr = []
+    res = []
+    for i in range(1, x+1):
+        arr.append(i)
+    num = 0
+
+
+    while arr:
+        num += + y - 1
+        if num > len(arr) -1:
+            num = num % len(arr)
+
+        res.append(str(arr.pop(num)))
+
+    print("<", ", ".join(res)[:], ">", sep='')
+
