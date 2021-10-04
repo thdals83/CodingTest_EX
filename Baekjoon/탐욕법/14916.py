bb0707cb3b0560d1f5ca8de0a7f2ff54a130b31c
thdals83 <thdals83@naver.com>
2021-10-04 16:09:22 +0900

    백준 알고리즘

---
+++ Baekjoon/탐욕법/14916.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+num = x % 5
+
+if x == 1 or x == 3:
+    print(-1)
+elif num % 2 == 0:
+    print(x//5 + num//2)
+else:
+    print(((x // 5) -1) + ((num + 5) //2))
