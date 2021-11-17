91cc4cd5ec458d5dfc910879689974ff676fcab6
thdals83 <thdals83@naver.com>
2021-11-17 12:10:17 +0900

    돌 게임

--- Baekjoon/DP2/15724.py
+++ Baekjoon/DP2/15724.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n, m = map(int,input().split())
 arr = []
 
@@ -12,7 +13,6 @@ dp = [[0 for i in range(m+1)] for _ in range(n+1)]
 for i in range(1,n+1):
     for j in range(1, m+1):
         dp[i][j] = dp[i-1][j] + dp[i][j-1] + arr[i-1][j-1] - dp[i-1][j-1]
-
 
 for _ in range(k):
     x1,y1,x2,y2 = map(int,input().split())
