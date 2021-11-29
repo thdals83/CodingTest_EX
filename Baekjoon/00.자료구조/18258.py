a764ddd30662dc9edfa38290da0cd267767ba58a
thdals83 <thdals83@naver.com>
2021-11-29 10:14:08 +0900

    큐 2

---
+++ Baekjoon/00.자료구조/18258.py
@@ -0,0 +1,28 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+queue = deque()
+for _ in range(n):
+    arr = list(input().split())
+    if arr[0] == "push":
+        queue.append(arr[1])
+    elif arr[0] == "pop":
+        if len(queue) == 0: print(-1)
+        else:
+            print(queue.popleft())
+    elif arr[0] == "size":
+        print(len(queue))
+    elif arr[0] == "empty":
+        if len(queue) == 0: print(1)
+        else: print(0)
+    elif arr[0] == "front":
+        if len(queue) == 0: print(-1)
+        else:
+            print(queue[0])
+    elif arr[0] == "back":
+        if len(queue) == 0: print(-1)
+        else:
+            print(queue[-1])
