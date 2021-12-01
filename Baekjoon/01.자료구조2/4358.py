4b8b64dc4e3f859d277668ec3d7a8e0019d7b1b1
thdals83 <thdals83@naver.com>
2021-12-01 10:33:34 +0900

    생태학

---
+++ Baekjoon/01.자료구조2/4358.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+dic = {}
+cnt = 0
+while True:
+    str = input().strip()
+    if not str: break
+
+    dic.setdefault(str, 0)
+    dic[str] += 1
+    cnt += 1
+
+arr = sorted(dic.keys())
+
+for i in arr:
+    print("%s %.4f" % (i, dic[i]*100/cnt))
