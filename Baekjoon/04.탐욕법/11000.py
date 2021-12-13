f0699ecdf4e366c7d8b35e77f9a8059ceeaccfe6
thdals83 <thdals83@naver.com>
2021-12-13 11:42:26 +0900

    강의실 배정

---
+++ Baekjoon/04.탐욕법/11000.py
@@ -0,0 +1,22 @@
+import heapq,sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+arr.sort()
+
+room = []
+heapq.heappush(room,arr[0][1])
+
+for i in range(1,n):
+    if arr[i][0] < room[0]:
+        heapq.heappush(room, arr[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, arr[i][1])
+
+print(len(room))
