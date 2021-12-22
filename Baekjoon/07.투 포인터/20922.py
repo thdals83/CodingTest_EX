f37bc124aa239d5fe6542dcf1c91e67e3ff17d88
thdals83 <thdals83@naver.com>
2021-12-22 14:06:24 +0900

    겹치는 건 싫어

--- Baekjoon/07.투 포인터/20922.py
+++ Baekjoon/07.투 포인터/20922.py
@@ -18,3 +18,21 @@ for i in range(len(arr)):
     # print(arr[i], len(count[arr[i]]))
 res = max(res, n - start)
 print(res)
+
+'''
+N, K = map(int, input().split())
+numbers = list(map(int, input().split()))
+left, right = 0, 0
+
+counter = [0] * (max(numbers) + 1)
+answer = 0
+while right < N:
+    if counter[numbers[right]] < K:
+        counter[numbers[right]] += 1
+        right += 1
+    else:
+        counter[numbers[left]] -= 1
+        left += 1
+    answer = max(answer, right - left)
+print(answer)
+'''
