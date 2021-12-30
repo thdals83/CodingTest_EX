b441ceb184f47c0017dca47c49ce8879121768b8
thdals83 <thdals83@naver.com>
2021-12-30 11:03:59 +0900

    베스트앨범

--- programmers/Hash/베스트앨범.py
+++ programmers/Hash/베스트앨범.py
@@ -1,24 +1,30 @@
+'''
+- 장르 별로 가장 많이 재생된 노래를 두 개씩 모아
+1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+'''
 
 def solution(genres,plays):
-    arr = {}
-    sum = {}
+    song = {}
+    maxgen = {}
     i = 0
-    for x,y in zip(genres,plays):
-        arr[x] = arr.get(x,[]) + [(y,i)]
-        sum[x] = sum.get(x,0) + y
+    for x, y in zip(genres,plays):
+        song[x] = song.get(x,[]) + [[y,i]]
+        maxgen[x] = maxgen.get(x, 0) + y
         i += 1
-    max = sorted(sum.items(), key=lambda x:x[1],reverse=True)
-    res =[]
-    for i in range(len(max)):
-        max2 = sorted(arr[max[i][0]], key=lambda x:x[0],reverse=True)
-        if len(max2) < 2:
-            res.append(max2[0][1])
+
+    sortgen = sorted(maxgen.items(), key = lambda x:x[1], reverse=True)
+    res = []
+    for i in sortgen:
+        sortsong = sorted(song[[i[0]][0]], key= lambda x:x[0], reverse= True)
+        if len(sortsong)  < 2:
+            res.append(sortsong[0][1])
         else:
-            res.append(max2[0][1])
-            res.append(max2[1][1])
+            res.append(sortsong[0][1])
+            res.append(sortsong[1][1])
 
     return res
-
 
 
 if __name__ =="__main__":
