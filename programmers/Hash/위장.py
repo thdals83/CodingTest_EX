34cc725e91291c183d7b3c657af0c05131f2ae93
thdals83 <thdals83@naver.com>
2021-12-30 11:21:13 +0900

    위장

--- programmers/Hash/위장.py
+++ programmers/Hash/위장.py
@@ -1,19 +1,17 @@
 
-
 def solution(clothes):
-    arr ={}
+    arr = {}
     for i in clothes:
         if i[-1] in arr:
             arr[i[-1]] += 1
         else:
             arr[i[-1]] = 1
 
-    sum = 1
+    res = 1
     for i in arr.values():
-        sum *= (i+1)
-    return sum -1
+        res *= (i+1)
 
-
+    return res -1
 
 if __name__ =="__main__":
     c=[["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
