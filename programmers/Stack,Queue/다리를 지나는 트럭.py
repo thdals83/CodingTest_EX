1c65ac0af466b6f958fb842be372968b3760fdfa
thdals83 <thdals83@naver.com>
2022-01-20 22:09:18 +0900

    다리를 지나는 트럭

--- programmers/Stack,Queue/다리를 지나는 트럭.py
+++ programmers/Stack,Queue/다리를 지나는 트럭.py
@@ -1,11 +1,12 @@
 from collections import deque
+
 # 경과시간 1씩 추가
 # 건너고 있는 트럭 (ing) 1씩 증가
 # 대기 트럭에 트럭이 있는 경우
 # => 현재 ing 와 대기트럭을 더했을 때 weight보다 작은 경우
 #  => ing.apppend 트럭(0)
 # 대기 트럭에 트럭이 없는 경우
-#
+
 def solution(length,weight,truck):
     answer = 1
     truck = deque(truck)
