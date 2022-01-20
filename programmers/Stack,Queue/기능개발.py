135942ad8a2bb252d9c71d0410116196e8fe48e1
thdals83 <thdals83@naver.com>
2022-01-20 12:41:18 +0900

    기능 개발

--- programmers/Stack,Queue/기능개발.py
+++ programmers/Stack,Queue/기능개발.py
@@ -1,30 +1,33 @@
+from collections import deque
 
 def solution(progresses,speeds):
     answer = []
-    boxs = []
-    for x,y in zip(progresses,speeds): boxs.append([x,y])
-    n = -1
-    while len(boxs) != 0:
-        check = True
-        num = 0
-        while num != len(boxs):
-            if boxs[num][0] < 100: break
-            else:
-                boxs.pop(num)
-                if not check:
-                    answer[n] += 1
-                else:
-                    answer.append(1)
-                    n += 1
-                    check = False
+    progresses = deque(progresses)
+    speeds = deque(speeds)
 
-        for box in boxs:
-            box[0] += box[1]
+    while progresses:
+        res = 1
+        progresse = progresses.popleft()
+        speed = speeds.popleft()
+
+        minu = (100 - progresse) //speed
+        if minu * speed + progresse < 100: minu += 1
+
+        while progresses:
+            if progresses[0] + (speeds[0] * minu) >= 100:
+                progresses.popleft()
+                speeds.popleft()
+                res += 1
+            else:
+                break
+
+        answer.append(res)
+
     return answer
 
 if __name__ =="__main__":
     p = [93, 30, 55]
     s= [1, 30, 5]
-    p1=[95, 90, 99, 99, 80, 99]
-    s1=[1, 1, 1, 1, 1, 1]
-    print(solution(p,s))
+    p1=  [99, 99, 99, 99, 99]
+    s1= [99, 99, 99, 99, 99]
+    print(solution(p1,s1))
