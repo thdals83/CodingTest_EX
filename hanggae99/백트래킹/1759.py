fe926b940f81ab3f7a4d97e5983f8167aa3c57f2
thdals83 <thdals83@naver.com>
2022-01-24 17:37:32 +0900

    암호 만들기

--- hanggae99/백트래킹/1759.py
+++ hanggae99/백트래킹/1759.py
@@ -1,4 +1,3 @@
-
 # 서로 다른 L개의 알파벳 소문자들
 # 최소 한 개의 모음(a, e, i, o, u)
 # 최소 두개의 자음으로 구성
@@ -17,6 +16,7 @@ def check(lv,idx, path):
 
     if l == len(path):
         tmp = path.copy()
+
         if findtrue(tmp):
             tmp.sort()
             #print(''.join(tmp))
@@ -32,6 +32,7 @@ def check(lv,idx, path):
 l, c = map(int,input().split())
 arr = list(input().split())
 final = []
+
 check(0,0,[])
 
 final.sort()
