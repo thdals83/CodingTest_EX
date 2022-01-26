d07375a1fdb71a5751e3ee7791640fa95ebb48c9
thdals83 <thdals83@naver.com>
2022-01-26 15:28:57 +0900

    가장 긴 동일 값의 경로

---
+++ hanggae99/트리/가장 긴 동일 값의 경로.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+# Definition for a binary tree node.
+from pyasn1.compat.octets import null
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def make_tree_by(lst, idx):
+    parent = None
+    if idx < len(lst):
+        value = lst[idx]
+        if value is None:
+            return
+
+        parent = TreeNode(value)
+        parent.left = make_tree_by(lst, 2 * idx + 1)
+        parent.right = make_tree_by(lst, 2 * idx + 2)
+    return parent
+
+# DFS 스택 사용
+class Solution:
+    result: int = 0
+
+    def longestUnivaluePath(self, root: TreeNode) -> int:
+        def dfs(node: TreeNode):
+            if node is None:
+                return 0
+
+            # 존재하지 않는 노드까지 DFS 재귀 탐색
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            # 현재 노드가 자식 노드와 동일한 경우 거리 1 증가
+            if node.left and node.left.val == node.val:
+                left += 1
+            else:
+                left = 0
+            if node.right and node.right.val == node.val:
+                right += 1
+            else:
+                right = 0
+
+
+            # 왼쪽, 오른쪽 자식 노드간 거리의 합 최대값이 결과
+            self.result = max(self.result, left + right)
+            # 자식 노드 상태값 중 큰 값 리턴
+            return max(left, right)
+
+        dfs(root)
+        return self.result
+
+
+if __name__ == "__main__":
+    x = Solution()
+    root = [5,4,5,1,1,5]
+    inputtree = make_tree_by(root, 0)
+    print(x.longestUnivaluePath(inputtree))
