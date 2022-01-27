db844518cc112ba8e6d6f1074e7fe76144756526
thdals83 <thdals83@naver.com>
2022-01-27 10:51:05 +0900

    방문 길이(프로그래머스)

---
+++ hanggae99/그래프/방문 길이(프로그래머스).py
@@ -0,0 +1,58 @@
+def solution(dirs):
+    answer = 0
+    visited = []
+    ing = [0,0]
+
+    for dir in dirs:
+        if dir == "U":
+            if ing[1] + 1 < 6:
+                back = [ing[0], ing[1], ing[0], ing[1] + 1]
+                back2 = [ing[0], ing[1] + 1, ing[0], ing[1]]
+                ing[1] += 1
+
+                if (back and back2) not in visited:
+                    answer += 1
+                    visited.append(back)
+                    visited.append(back2)
+
+        elif dir == "D":
+            if -6 < ing[1] - 1:
+                #print("d", end = " ")
+                back = [ing[0], ing[1], ing[0], ing[1] - 1]
+                back2 = [ing[0], ing[1] - 1, ing[0], ing[1]]
+                ing[1] -= 1
+
+                if (back and back2) not in visited:
+                    answer += 1
+                    visited.append(back)
+                    visited.append(back2)
+
+        elif dir == "R":
+            if ing[0] + 1 < 6:
+                back = [ing[0], ing[1], ing[0] + 1, ing[1]]
+                back2 = [ing[0] + 1, ing[1], ing[0], ing[1]]
+                ing[0] += 1
+
+                if (back and back2) not in visited:
+                    answer += 1
+                    visited.append(back)
+                    visited.append(back2)
+
+        elif dir == "L":
+            if -6 < ing[0] - 1:
+
+                back = [ing[0], ing[1], ing[0] - 1, ing[1]]
+                back2 = [ing[0] - 1, ing[1], ing[0], ing[1]]
+                ing[0] -= 1
+
+                if (back and back2) not in visited:
+                    answer += 1
+                    visited.append(back)
+                    visited.append(back2)
+
+    return answer
+
+if __name__ == "__main__":
+    d = "ULURRDLLU"
+    d2 = "LLLLRLRLRLL"
+    print(solution(d))
