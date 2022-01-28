f3420e0b8f10fbdbc3d57bd69b3d609a59fdc4b9
thdals83 <thdals83@naver.com>
2022-01-28 20:18:17 +0900

    최소 힙

--- hanggae99/힙/1927.py
+++ hanggae99/힙/1927.py
@@ -1,17 +1,15 @@
 import heapq
-import sys
-input = sys.stdin.readline
 
 n = int(input())
 heap = []
 
 for _ in range(n):
     x = int(input())
+
     if x == 0:
         if len(heap) == 0:
             print(0)
         else:
             print(heapq.heappop(heap))
-
     else:
-        heapq.heappush(heap,x)
+        heapq.heappush(heap, x)
