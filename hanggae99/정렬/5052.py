565c62bda402f121061078999028bc64de749638
thdals83 <thdals83@naver.com>
2022-01-29 15:34:46 +0900

    전화번호 목록

---
+++ hanggae99/정렬/5052.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def check(res):
+    res.sort()
+    for i in range(len(res) - 1):
+        if res[i] in res[i + 1][:len(res[i])]:
+            return "NO"
+    return "YES"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = []
+
+    for _ in range(n):
+        arr.append(input().strip())
+
+    print(check(arr))
