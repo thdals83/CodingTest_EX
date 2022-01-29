18b25d268fbabe8cb872bf06b0e407db2abf0e41
thdals83 <thdals83@naver.com>
2022-01-29 14:21:59 +0900

    삽입정렬 리스트

--- hanggae99/정렬/삽입 정렬 리스트.py
+++ hanggae99/정렬/삽입 정렬 리스트.py
@@ -9,17 +9,17 @@ class Solution:
     def insertionSortList(self, head: ListNode) -> ListNode:
         # 초기값 변경
         cur = parent = ListNode(0)
+
         while head:
             while cur.next and cur.next.val < head.val:
                 cur = cur.next
 
-            cur.next, head.next, head = head, cur.next, head.next
+            head, cur.next, head.next = head.next, head, cur.next
 
-            # 필요한 경우에만 cur 포인터가 되돌아가도록 처리
             if head and cur.val > head.val:
                 cur = parent
-        return parent.next
 
+        return parent.next
 '''
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
