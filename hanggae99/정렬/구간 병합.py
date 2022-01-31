7267dd2e4d57994a0c6553ba8e9a1c3060d04e37
thdals83 <thdals83@naver.com>
2022-01-31 15:22:50 +0900

    구간 병합

---
+++ hanggae99/정렬/구간 병합.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key= lambda x:x[0])
+        res = [intervals[0]]
+        idx = 0
+        for s,e in intervals[1:]:
+            if res[idx][1] >= e:
+                continue
+            if res[idx][1] >= s:
+                res[idx][1] = e
+            else:
+                res.append([s,e])
+                idx += 1
+
+        return res
+
+
+if __name__  == "__main__":
+    x = Solution()
+    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+    print(x.merge(intervals))
