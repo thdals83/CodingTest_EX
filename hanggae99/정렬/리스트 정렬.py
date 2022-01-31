9f513b5e38141f5c9451d122f8f7eb9df7cf147a
thdals83 <thdals83@naver.com>
2022-01-31 10:45:37 +0900

    리스트 정렬

---
+++ hanggae99/정렬/리스트 정렬.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        arr = []
+
+        while node:
+            arr.append(node.val)
+            node = node.next
+
+        arr.sort()
+
+        node = head
+        for n in arr:
+            node.val = n
+            node = node.next
+
+        return head
