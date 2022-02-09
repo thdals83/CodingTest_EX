8026381617f0f0878cf91c9cac96eac82c8e51c1
thdals83 <thdals83@naver.com>
2022-02-09 17:15:45 +0900

    계단 오르기

--- hanggae99/DP/계단 오르기.py
+++ hanggae99/DP/계단 오르기.py
@@ -2,6 +2,7 @@ from typing import List
 import sys
 from collections import defaultdict
 
+'''
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = defaultdict(int)
@@ -14,9 +15,22 @@ class Solution:
             dp[i] = dp[i - 1] + dp[i - 2]
 
         return dp[n]
+'''
 
+class Solution:
+    dp = defaultdict(int)
+
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+
+        self.dp[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        return self.dp[n]
 
 if __name__ == "__main__":
     x = Solution()
-    n = 3
+    n = 6
     print(x.climbStairs(n))
