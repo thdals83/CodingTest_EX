e9f038c524b08cbd5265ea8431501e0e27ca6b4e
thdals83 <thdals83@naver.com>
2022-02-10 10:23:38 +0900

    입국심사(프로그래머스)

---
+++ hanggae99/이진 탐색/입국심사(프로그래머스).py
@@ -0,0 +1,25 @@
+
+def solution(n, times):
+    times.sort()
+    start, end = 1, times[-1] * n
+
+    while start <= end:
+        mid = (start + end) // 2
+        sum = 0
+
+        for time in times:
+            sum += mid // time
+
+        if sum < n:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return start
+
+
+
+if __name__ == "__main__":
+    n = 6
+    times = [7, 10]
+    print(solution(n,times))
