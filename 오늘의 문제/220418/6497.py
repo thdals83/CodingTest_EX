a2df87d44035e50e969631c50943b594d38780a9
thdals83 <thdals83@naver.com>
2022-04-19 09:21:17 +0900

    전력난

--- 오늘의 문제/220418/6497.py
+++ 오늘의 문제/220418/6497.py
@@ -1,11 +1,9 @@
 import sys
-input = sys.stdin.readline
-
+# input = sys.stdin.readline
 
 def find_parent(x, parent):
     if parent[x] != x:
-        parent[x] = find_parent(parent[x],parent)
-
+        parent[x] = find_parent(parent[x], parent)
     return parent[x]
 
 def union_parent(a, b, parent):
@@ -21,20 +19,19 @@ if __name__ == "__main__":
         m, n = map(int, input().split())
         if m == 0 and n == 0: break
 
-        parent = [i for i in range(m)]
+        parents = [i for i in range(m)]
         edges = []
         for _ in range(n):
-            x, y, z = map(int,input().split())
-            edges.append((z, x, y))
+            x,y,z = map(int,input().split())
+            edges.append((z,x,y))
 
         edges.sort()
         total = 0
 
         for i in range(len(edges)):
             z, x, y = edges[i]
-            if find_parent(x, parent) != find_parent(y, parent):
-                union_parent(x,y,parent)
+            if find_parent(x,parents) != find_parent(y,parents):
+                union_parent(x,y,parents)
             else:
                 total += z
-
         print(total)
