13d28cacefe650f791bbe1c9dd7bc978fb79d1f6
thdals83 <thdals83@naver.com>
2022-04-19 09:38:53 +0900

    설탕 배달

--- hanggae99/알고리즘 스터디/기본 수학/2839.py
+++ hanggae99/알고리즘 스터디/기본 수학/2839.py
@@ -3,8 +3,8 @@ N = int(input())
 con5 = -1
 con3 = 0 
 
-for i in range (int(N/5),-1,-1):
-    if (N - 5*i) %3 ==0:
+for i in range (int(N/5), -1, -1):
+    if (N - 5 * i) % 3 == 0:
         con5 = i
         con3 = int((N - 5*i)/3) 
         break
