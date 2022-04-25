98d08df97e0c852a1946d834c974273c31f801f6
thdals83 <thdals83@naver.com>
2022-04-25 14:05:48 +0900

    버블, 삽입, 선택

--- hanggae99/정렬/버블,삽입,선택.py
+++ hanggae99/정렬/버블,삽입,선택.py
@@ -1,35 +1,32 @@
 
 '''
 버블 정렬 O(N^2)
-for문을 두번 돌려주면서, 인접한 두 원소를 비교해 정렬
+버블 정렬은 앞에 수와 뒤의 수를 비교해 정렬해 주는 것입니다.
 '''
 def bubblesort(nums):
-    for i in range(len(nums) - 1):
+    for i in range(len(nums)):
         for j in range(len(nums) - 1):
-            if nums[j] < nums[j + 1]:
-                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+            if nums[i] < nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
     return nums
-
 
 '''
 선택 정렬 O(N^2)
-가장 작은 수를 선택해 앞에서 부터 순차적으로 바꿔주는 것
+전체 배열에서 가장 작은 수를 선택해 앞에서 부터 차례대로 바꿔 줍니다.
 '''
 def selectesort(nums):
-    for i in range(len(nums) - 1):
+    for i in range(len(nums)):
         tmp = i
         for j in range(i + 1, len(nums)):
             if nums[tmp] > nums[j]:
                 tmp = j
-
         if tmp != i:
             nums[i], nums[tmp] = nums[tmp], nums[i]
-
     return nums
 
 '''
 삽입 정렬 O(N^2)
-순차적으로 두번째 수부터, 원소를 골라 앞에서 부터 비교해서 삽입하는 형식
+두번째 수 부터 차례로 수를 골라 앞에서 비교하면서 적절하 위치에 삽입해 주는 것입니다.
 '''
 def insertesort(nums):
     for i in range(1, len(nums)):
@@ -38,7 +35,6 @@ def insertesort(nums):
                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
             else:
                 break
-
     return nums
 
 
