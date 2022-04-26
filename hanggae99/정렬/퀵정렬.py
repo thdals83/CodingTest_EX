99a237551f4df0e9756a6f0ca818eee4b45214c4
thdals83 <thdals83@naver.com>
2022-04-26 14:59:22 +0900

    퀵 정렬

--- hanggae99/정렬/퀵정렬.py
+++ hanggae99/정렬/퀵정렬.py
@@ -1,32 +1,37 @@
 '''
-퀵정렬 평균: O(NlogN),
-최악: O(N^2) => 이미 정렬된 배열에 대해서는 효과가 좋지 않다.
-피벗값을 기준으로 큰 데이터와 작은 데이터의 위치를 바꾸다가 위치가 엇갈리는 경우
-피벗값의 위치와 변경 시켜주고 피벗값을 기준으로 재귀가 돌면서 계속 이를 반복해준다.
+퀵정렬 평균: O(NlogN)
+최악: O(N^2) => 만약 정렬이 되어 있는 배열에 경우 피벗이 효율적이게 작동하지 않는다
+사용법
+피벗을 중심으로 오름차순을 한다고 가정할때, 왼쪽은 피벗보다 값이 큰값, 오른쪽은 피벗보다 값이 작은 값을 선택해
+위치를 바꿔준다. 만약 왼쪽과 오른쪽이 서로 크로스 되게 되면 작은 값과 피벗을 바꿔주고
+이 과정을 재귀로 반복해서 돌게 된다.
 '''
-def queicsort(array, start, end):
+def queicsort(arr, start, end):
     if start >= end: return
     pivot = start
     left = start + 1
     right = end
 
+    #엇갈리기 전까지
     while left < right:
-        while left <= end and array[left] <= array[pivot]:
+        # left 수 찾기
+        while left <= end and arr[left] <= arr[pivot]:
             left += 1
-        while right > start and array[right] >= array[pivot]:
+        # right 수 찾기
+        while right > start and arr[right] >= arr[pivot]:
             right -= 1
 
         if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
+            arr[right], arr[pivot] = arr[pivot], arr[right]
         else:
-            array[left], array[right] = array[right], array[left]
+            arr[left], arr[right] = arr[right], arr[left]
 
-    queicsort(array,start,right - 1)
-    queicsort(array,right + 1, end)
+    queicsort(arr,start, right - 1)
+    queicsort(arr, right + 1, end)
 
 
 
 if __name__  == "__main__":
-    array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
-    queicsort(array, 0, len(array) -1)
-    print(array)
+    arr = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+    queicsort(arr, 0, len(arr) - 1)
+    print(arr)
