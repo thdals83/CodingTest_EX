80d4dc54458c8ccd4bbaab3e7962175fbcd1529e
thdals83 <thdals83@naver.com>
2022-04-26 15:27:39 +0900

    병합 정렬

--- hanggae99/정렬/병합 정렬.py
+++ hanggae99/정렬/병합 정렬.py
@@ -1,36 +1,36 @@
 '''
 병합정렬 두개가 남을때까지 쪼개고 이를 계쏙 합치면서 정렬 O(NlogN)
 '''
-def merge(arr1, arr2):
-    result = []
+
+def merge(left, right):
+    res = []
     i = j = 0
-    while i < len(arr1) and j < len(arr2):
-        if arr1[i] < arr2[j]:
-            result.append(arr1[i])
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            res.append(left[i])
             i += 1
         else:
-            result.append(arr2[j])
+            res.append(right[j])
             j += 1
 
-    while i < len(arr1):
-        result.append(arr1[i])
+    while i < len(left):
+        res.append(left[i])
         i += 1
-
-    while j < len(arr2):
-        result.append(arr2[j])
+    while j < len(right):
+        res.append(right[j])
         j += 1
 
-    return result
-
+    return res
 
 def mergesort(lst):
-    if len(lst) <= 1:
-        return lst
+    if len(lst) <= 1: return lst
 
     mid = len(lst) // 2
     L = lst[:mid]
     R = lst[mid:]
     return merge(mergesort(L), mergesort(R))
+
 
 if __name__  == "__main__":
     array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
