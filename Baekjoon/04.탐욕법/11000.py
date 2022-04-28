80d6ae28c53c3bd527befe6a5224376012e96dcb
thdals83 <thdals83@naver.com>
2022-04-28 15:30:50 +0900

    강의실 배정

--- Baekjoon/04.탐욕법/11000.py
+++ Baekjoon/04.탐욕법/11000.py
@@ -1,22 +1,29 @@
-import heapq,sys
+
+'''
+s에서 시작해 t로 끝나는 n개의 수업
+최소의 강의실 사용해 모든 수업 가능하게 해야함
+
+수업이 끝난 직후 다음 수업ㅇㄹ 시작
+'''
+import heapq
+import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = []
-
+studys = []
 for _ in range(n):
-    arr.append(list(map(int,input().split())))
+    s, t = map(int,input().split())
+    studys.append([s, t])
 
-arr.sort()
+studys = sorted(studys, key=lambda x:x[0])
 
-room = []
-heapq.heappush(room,arr[0][1])
+rooms = []
+heapq.heappush(rooms, studys[0][1])
 
-for i in range(1,n):
-    if arr[i][0] < room[0]:
-        heapq.heappush(room, arr[i][1])
+for i in range(1, len(studys)):
+    if studys[i][0] < rooms[0]:
+        heapq.heappush(rooms, studys[i][1])
     else:
-        heapq.heappop(room)
-        heapq.heappush(room, arr[i][1])
-
-print(len(room))
+        heapq.heappop(rooms)
+        heapq.heappush(rooms, studys[i][1])
+print(len(rooms))
