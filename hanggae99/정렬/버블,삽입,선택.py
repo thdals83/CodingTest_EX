e0a2a3773955eb7dbd2857ed64f7a14bed924e5b
thdals83 <thdals83@naver.com>
2022-05-05 15:07:48 +0900

    버블 삽입 선택

--- hanggae99/정렬/버블,삽입,선택.py
+++ hanggae99/정렬/버블,삽입,선택.py
@@ -1,11 +1,10 @@
-
 '''
 버블 정렬 O(N^2)
 이중 반복문을 돌면서 앞 뒤 수를 비교해 정렬해줍니다.
 '''
 def bubblesort(nums):
     for i in range(len(nums)):
-        for j in range(len(nums)):
+        for j in range(len(nums) - 1):
             if nums[i] < nums[j]:
                 nums[i], nums[j] = nums[j], nums[i]
     return nums
@@ -24,7 +23,6 @@ def selectesort(nums):
             nums[i], nums[tmp] = nums[tmp], nums[i]
     return nums
 
-
 '''
 삽입 정렬 O(N^2)
 첫번째 수는 정렬이 되었다고 가정하고, 두번째 수부터 앞에 수들과 비교를 해가며, 적절한 위치에 삽입해 줍니다.
@@ -33,13 +31,11 @@ def insertesort(nums):
     for i in range(1, len(nums)):
         for j in range(i, 0, -1):
             if nums[j] < nums[j - 1]:
-                nums[j], nums[j - 1], = nums[j - 1], nums[j]
-            else:
+                nums[j], nums[j - 1] = nums[j - 1], nums[j]
                 break
     return nums
 
 
 if __name__ == "__main__":
-    nums = [3,30,34,5,9]
+    nums = [3, 30, 34, 5, 9]
     print(insertesort(nums))
-
