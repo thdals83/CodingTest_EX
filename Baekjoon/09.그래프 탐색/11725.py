5f17bb872d1b2ff57e4c5da082c3bbb7e46f8218
thdals83 <thdals83@naver.com>
2022-05-06 14:59:47 +0900

    트리의 부모 찾기

--- Baekjoon/09.그래프 탐색/11725.py
+++ Baekjoon/09.그래프 탐색/11725.py
@@ -1,24 +1,31 @@
+from collections import defaultdict
 from collections import deque
 import sys
 input = sys.stdin.readline
+'''
+트리의 루트 = 1
+각 노드의 부모를 구하라
+'''
 
 n = int(input())
-res = [0] * (n+1)
-arr = [[] for _ in range(n+1)]
-queue = deque()
+tree = defaultdict(list)
 
-for _ in range(n-1):
-    x,y = map(int,input().split())
-    arr[x].append(y)
-    arr[y].append(x)
+for _ in range(n - 1):
+    x, y = map(int,input().split())
+    tree[x].append(y)
+    tree[y].append(x)
 
-queue.append(1)
+visited = [0] * (n + 1)
+queue = deque([1])
+visited[1] = 1
+
 while queue:
     node = queue.popleft()
-    for i in arr[node]:
-        if res[i] == 0:
-            res[i] = node
-            queue.append(i)
 
-for i in range(2,n+1):
-    print(res[i])
+    for i in tree[node]:
+        if visited[i] == 0:
+            queue.append(i)
+            visited[i] = node
+
+for i in range(2, len(visited)):
+    print(visited[i])
