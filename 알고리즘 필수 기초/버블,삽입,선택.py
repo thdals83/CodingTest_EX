12c78fbbb27ecdd3370d42fdea9f6b043e708158
thdals83 <thdals83@naver.com>
2022-05-10 13:36:23 +0900

    버블, 삽입, 선택

--- 알고리즘 필수 기초/버블,삽입,선택.py
+++ 알고리즘 필수 기초/버블,삽입,선택.py
@@ -32,6 +32,7 @@ def insertesort(nums):
         for j in range(i, 0, -1):
             if nums[j] < nums[j - 1]:
                 nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            else:
                 break
     return nums
 
